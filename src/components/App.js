ca863f379a7e7c44c709ac89a0a3b29a9af3a249
Rohit Panjwani <iamrohit.panjwani@gmail.com>
2020-10-31 18:46:34 +0530

    try sometihng new

--- src/components/App.js
+++ src/components/App.js
@@ -6,30 +6,53 @@ function App()
 
 	const [tasks, setTasks] = React.useState([]);
 	const task = React.useRef();
+	const [taskString, setTaskString] = React.useState('');
+	const [editedTaskString, setEditedTaskString] = React.useState('');
 	const editedTask = React.useRef();
 	const [selectedTask, setSelectedTask] = React.useState(null);
 
+	// const saveTask = () => {
+	// 	if (task.current.value === '') return;
+	// 	const value = task.current.value;
+	// 	task.current.value = '';
+	// 	setTasks(tasks => [...tasks, {
+	// 		id: value + tasks.length,
+	// 		name: value
+	// 	}])
+	// }
+
 	const saveTask = () => {
-		if (task.current.value === '') return;
-		const value = task.current.value;
-		task.current.value = '';
+		if (taskString === '') return;
 		setTasks(tasks => [...tasks, {
-			id: value + tasks.length,
-			name: value
+			id: taskString + tasks.length,
+			name: taskString
 		}])
+		setTaskString('');
 	}
 
+	// const editTask = (id) => {
+	// 	if (editedTask.current.value === '') return;
+	// 	const value = editedTask.current.value;
+	// 	editedTask.current.value = '';
+	// 	let tasksRep = tasks.map(task => {
+	// 		let newTask = {...task}
+	// 		if(task.id === id) newTask.name = value;
+	// 		return newTask
+	// 	})
+	// 	setTasks(tasks => tasksRep)
+	// 	setSelectedTask(null);
+	// }
+
 	const editTask = (id) => {
-		if (editedTask.current.value === '') return;
-		const value = editedTask.current.value;
-		editedTask.current.value = '';
+		if (editedTaskString === '') return;
 		let tasksRep = tasks.map(task => {
 			let newTask = {...task}
-			if(task.id === id) newTask.name = value;
+			if(task.id === id) newTask.name = editedTaskString;
 			return newTask
 		})
 		setTasks(tasks => tasksRep)
 		setSelectedTask(null);
+		setEditedTaskString('');
 	}
 
 	const deleteTask = (id) => {
@@ -41,36 +64,42 @@ function App()
 		setSelectedTask(id)
 	}
 
+	const handleTaskChange = (e) => {
+		let value = e.target.value;
+		setTaskString(value);
+	}
+
+	const handleEdit = (e) => {
+		let value = e.target.value;
+		setEditedTaskString(value);
+	}
+
 	return (
 	<div id="main">
 	//Do not alter main div
 	//Please do not alter the functional component as tests depend on the type of component.
-	<textarea id="task" ref={task} rows="4" cols="50"></textarea>
+	<textarea id="task" value={taskString} onChange={handleTaskChange} ref={task} rows="4" cols="50"></textarea>
 	<button id="btn" onClick={saveTask}>save</button>
-	<div>
+	<ol>
 		{
 			tasks.map(task => (
-				<div  key={task.id}>
-					<div className="list">
-						{task.name}
-					</div>
-					<div>
-						<button className="delete" onClick={() => deleteTask(task.id)}>delete</button>
-						<button className="edit" onClick={() => edit(task.id)}>edit</button>
-						{
-							selectedTask === task.id ? 
-							<>
-							<textarea ref={editedTask} className="editTask" rows="4" cols="50"></textarea>
-							<button className="saveTask" onClick={() => editTask(task.id)}>save</button>
-							</>
-							:
-							null
-						}
-					</div>
-				</div>
+				<li key={task.id}>
+					<span className="list">{task.name}</span>
+					<button className="delete" onClick={() => deleteTask(task.id)}>delete</button>
+					<button className="edit" onClick={() => edit(task.id)}>edit</button>
+					{
+						selectedTask === task.id ? 
+						<>
+						<textarea ref={editedTask} value={editedTaskString} onChange={handleEdit} className="editTask" rows="4" cols="50"></textarea>
+						<button className="saveTask" onClick={() => editTask(task.id)}>save</button>
+						</>
+						:
+						null
+					}
+				</li>
 			))
 		}
-	</div>
+	</ol>
 	</div>
 	);
 }
